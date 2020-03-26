e205b01e820fe7ba10940575de179779ad5bf384
Osman Goni <32338711+goniusman@users.noreply.github.com>
2020-03-26 13:18:31 +0600

    Update jspractice.js

--- jspractice.js
+++ jspractice.js
@@ -39,3 +39,17 @@ if (questionsearch) {
 	});
 }
 
+// create a div and show anywhre for alert.
+showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+  }
+
+
